634525536c8718e46965c882b9687d1ff9324b46
MITLE114514 <54273232+MITLE114514@users.noreply.github.com>
2021-12-10 14:39:39 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import numpy as np
-import requests
-from os import getcwd
 from pathlib import Path
 from torch.utils.data import Dataset
-
 
 class MLDataset(Dataset):
     def __init__(self):
